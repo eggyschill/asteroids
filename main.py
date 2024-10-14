802d2d6ab218f3ec57294985a01d594f2a76fae4
eggyschill <eleopeleram@gmail.com>
2024-10-14 11:31:59 -0700

    Update documentation, time tracking

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ from constants import *
 
 
 def main():
+    # Initialize pygame
     pygame.init()
+    # Set screen (display) width and height
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    # Gane loop
+    # Creating clock object for time tracking, and delta time variable
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Gane loop, infinite
     while (True):
+        # Setting parameter for quitting the program
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Otherwise, fill the screen with black
         screen.fill((0, 0, 0))  
+        # Update the display at 60 FPS, store that in a delta time variable in seconds
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
-    
 
 if __name__ == "__main__":
     main()
